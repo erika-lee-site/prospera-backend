e163ece8c08022cd11efa47fecd204e9b3777f8e
erika-lee-site <169239308+erika-lee-site@users.noreply.github.com>
2024-08-01 12:55:50 -0700

    Remove fetch on initialization

--- novu/workflows.js
+++ novu/workflows.js
@@ -3,24 +3,10 @@ const { z } = require('zod');
 const axios = require('axios');
 
 const hourlyHeadlinesWorkflow = workflow('hourly-headlines', async ({ step }) => {
-    const fetchHeadlines = async () => {
-        const response = await axios.get('https://newsapi.org/v2/everything', {
-            params: {
-                q: 'finance',
-                language: 'en',
-                sortBy: 'publishedAt',
-                pageSize: 9,
-                apiKey: process.env.NEWS_API_KEY,
-            },
-        });
-        return response.data.articles.map((article) => article.title);
-    };
-
-    const headlines = await fetchHeadlines();
 
     await step.inApp('send-headlines', async () => {
         return {
-            body: `Here are the latest headlines:\n\n${headlines.join('\n')}`,
+            body: `Here are the latest headlines:\n\n${[].join('\n')}`,
         };
     });
 }, {
